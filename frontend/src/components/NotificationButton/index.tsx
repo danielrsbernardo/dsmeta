6dbf936f2d917916fd894e7a0013286ea2e215c7
Daniel Bernardo <danielrsbernardo@gmail.com>
2022-07-11 09:26:53 -0300

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import Icon from '../../assets/img/icon-notification.svg'
+import'./style.css'
+function NotificationButton() {
+    return (
+        <>
+            <div className="dsmeta-red-btn">
+                <img src={Icon} alt="notificar"/>
+            </div>
+        </>
+    )
+
+}
+
+export default NotificationButton
