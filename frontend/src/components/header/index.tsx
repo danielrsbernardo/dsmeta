b36c2721b73ca0a559b987e493fb0ab6506ad4fc
Daniel Bernardo <danielrsbernardo@gmail.com>
2022-07-11 09:53:09 -0300

    Others Components

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import Logo from '../../assets/img/logo.svg'
+import './style.css'
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={Logo} alt="dsmeta"/>
+                        <h1>DSMeta</h1>
+                        <p>Desenvolvido por <a target="_blank"
+                            href="https://www.instagram.com/danielrsbernardo/">@danielrsbernardo</a>
+                        </p>
+                </div>
+            </header>
+        </>
+    )
+
+}
+
+export default Header
